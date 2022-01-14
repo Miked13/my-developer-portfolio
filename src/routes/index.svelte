ba635c46b19f9241472e521b75678634820bda44
Mike Diallo <diallomikey13@yahoo.fr>
2022-01-14 16:09:33 +0000

    Add first component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,13 +2,11 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <script context="module">
-    import { gql, GraphQLClient} from 'graphql-request'
+    import ProjectCard from '$lib/components/project-card.svelte'
+    import { client } from '$lib/graphql-client'
+    import { gql } from 'graphql-request'
 
     export const load = async() => {
-        const client = new GraphQLClient(
-            import.meta.env.VITE_GRAPHQL_API
-        )
-
         const query = gql`
         query GetProjects {
             projects{
@@ -37,4 +35,10 @@
     export let projects
 </script>
 
-<pre>{JSON.stringify(projects, null, 2)}</pre>
+<h1>Recent Projects by Me</h1>
+
+<div>
+    {#each projects as { name, slug, description, image }}
+    <ProjectCard {name} {description} url={image[0].url} {slug} />
+    {/each}
+</div>
