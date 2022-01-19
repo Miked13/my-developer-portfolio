72571acc77539460514071e27b1aa9495634ae75
Mike Diallo <diallomikey13@yahoo.fr>
2022-01-19 17:55:33 +0000

    Add posts index page and slug page

--- src/lib/graphql-queries.js
+++ src/lib/graphql-queries.js
@@ -42,3 +42,34 @@ query GetProject($slug: String!) {
         ...ProjectDetails
     }
 }`
+
+const POST_FRAGMENT = gql`
+  fragment PostDetails on Post {
+    title
+    slug
+    date
+    content
+    tags
+    coverImage {
+      url
+    }
+    authors {
+      name
+    }
+  }
+`
+export const postsQuery = gql`
+${POST_FRAGMENT}
+query GetPosts($slug: String!){
+    posts {
+        ...PostDetails
+    }
+}`
+
+export const postQuery = gql`
+${POST_FRAGMENT}
+query GetPost($slug: String!){
+    post(where: { slug: $slug }) {
+        ...PostDetails
+    }
+}`
